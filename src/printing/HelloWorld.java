e591b69a3c9b23a6b4d907e48ac05edf5a14a814
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-03 12:09:43 +0200

    skrev namn osv
    jag skrev ett namn med ett underslag

--- src/printing/HelloWorld.java
+++ src/printing/HelloWorld.java
@@ -3,8 +3,8 @@ package printing;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-       System.out.print("Päron,");
-       System.out.println(" Päron!");
+       System.out.print("Hello"); 
+       System.out.println("\n Alexandra Abramov");
 
 	}
 
