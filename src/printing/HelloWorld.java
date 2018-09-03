8957a40936e5f5cbfa77d3c1c2f11109e80be811
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-03 12:12:53 +0200

    skrev java stort med en print
    skrev java stort med en print

--- src/printing/HelloWorld.java
+++ src/printing/HelloWorld.java
@@ -3,8 +3,11 @@ package printing;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-       System.out.print("Hello"); 
-       System.out.println("\n Alexandra Abramov");
+       System.out.print("     J    a   v     v  a                                                  \r\n" + 
+       		       "     J   a a   v   v  a a                                                 \r\n" + 
+       	          	"  J  J  aaaaa   V V  aaaaa                                                \r\n" + 
+       		      "   JJ  a     a   V  a     a"); 
+       
 
 	}
 
