74f96f93d592e07bc4808e6840a5c95092989da3
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-10 11:16:12 +0200

    gjorde en flagga

--- src/printing/HelloWorld.java
+++ src/printing/HelloWorld.java
@@ -3,10 +3,8 @@ package printing;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-       System.out.print("     J    a   v     v  a                                                  \r\n" + 
-       		       "     J   a a   v   v  a a                                                 \r\n" + 
-       	          	"  J  J  aaaaa   V V  aaaaa                                                \r\n" + 
-       		      "   JJ  a     a   V  a     a"); 
+		System.out.print("HelloWorld"); 
+	      
        
 
 	}
