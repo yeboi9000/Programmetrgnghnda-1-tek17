e48b9f5d035ba08d48b353d7df4f9804395b9d40
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-10 11:21:37 +0200

    skrev en såndär grej om stjärnor

--- src/printing/Greeting.java
+++ src/printing/Greeting.java
@@ -4,10 +4,11 @@ public class Greeting {
 
 
 public static void main (String args[]) {
-System.out.println ("   +\"\"\"\"\"+                                                 \r\n" + 
-		"  [| o o |]                                                \r\n" + 
-		"   |  ^  |                                                 \r\n" + 
-		"   | '-' |                                                 \r\n" + 
-		"   +-----+");
+System.out.println ("Twinkle, twinkle, litte star,\r\n" + 
+		"   How I wonder what you are! \r\n" + 
+		"    Up above the world so high,     \r\n" + 
+		"    Like a diamond in the sky. \r\n" + 
+		"  Twinkle, twinkle, little star, \r\n" + 
+		"   How I wonder what yolu are");
 }
 }
