87f3f91b2845a3d0951d3c4825702f4ca544f686
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-14 09:04:45 +0200

    uuh

--- src/printing/Sumnumber.java
+++ src/printing/Sumnumber.java
@@ -3,7 +3,10 @@ package printing;
 public class Sumnumber {
 
 	public static void main(String[] args) {
-               System.out.print(50 / 3);
+               System.out.print(-5 + 8 * 6);
+               System.out.print(55+5 % 9);
+               System.out.print(20 + -3*5 / 8);
+               System.out.print(5 + 15 / 3 * 2 - 8 % 3 );
                
 	}
 
