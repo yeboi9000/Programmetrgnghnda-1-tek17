2f7d5afc72414314e737dcf24f5afbb7856f74ea
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-14 08:57:07 +0200

    skrev delat

--- src/printing/Sumnumber.java
+++ src/printing/Sumnumber.java
@@ -3,7 +3,7 @@ package printing;
 public class Sumnumber {
 
 	public static void main(String[] args) {
-               System.out.print(74 + 36);
+               System.out.print(50 / 3);
                
 	}
 
