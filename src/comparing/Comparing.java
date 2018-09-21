9cd6dd40f8751a05a1cdde76e952590e0c50e8c3
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-21 10:17:07 +0200

    romerska nummer
    gjorde inte lika många som du men de fungerar likadant

---
+++ src/comparing/Comparing.java
@@ -0,0 +1,32 @@
+package comparing;
+
+public class Comparing {
+
+	public static void main(String[] args) {
+		String romanNumber = "XXVVVVVIIIIIIILLL";
+		int number = 0;
+		
+	    for (int index = 0; index <romanNumber.length(); index++ ) {
+		System.out.println("index " + index);													
+	    if (romanNumber.charAt(index) == 'I') {
+	 	    number = number + 1;
+	 	}
+	 	if (romanNumber.charAt(index) == 'V') {
+	 		number = number + 50;
+	 	}
+	 	if (romanNumber.charAt(index) == 'X') {
+		 	number = number + 500;
+	 		
+	    }
+		if (romanNumber.charAt(index) == 'L') {
+		 	number = number + 5000;
+	 		
+	    }
+		System.out.println(number); 
+
+	    }
+
+	}
+}
+
+
