734f556fb5226a8bc8240845fd790759bf7384a6
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-24 12:03:06 +0200

    java test
    if satser osvosv

---
+++ src/input/InputTest.java
@@ -0,0 +1,47 @@
+package input;
+
+import java.util.Scanner;
+
+public class InputTest {
+
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in); 
+         
+		System.out.print("Enter text: ");
+         
+          inputScanner.nextLine();
+          
+          String name = inputScanner.nextLine();
+         
+          System.out.println("Hello" + name);
+          
+          System.out.print("Enter your age: ");
+          
+          int age = inputScanner.nextInt();
+          
+          if (age > 120) {
+        	  System.out.println("You lying piece of shit!!!!");
+        	  System.exit(0); 
+          } else if(age < 5) {
+        	System.out.println("You must be older!");
+          System.exit(0);
+          } else {
+          System.out.println("So your age is:" + age);
+          }
+          
+         
+          System.out.println("Enter your weight: ");
+          double weight = inputScanner.nextDouble();
+          if (weight > 400) {
+          System.out.println("fatty"); 
+          } else if(weight < 50) {
+        	  System.out.println("Skinnyboi");
+          }
+          System.out.println("So your weight is:" + weight);
+          
+          
+        
+	}
+	
+
+}
