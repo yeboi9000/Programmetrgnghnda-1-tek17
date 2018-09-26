79a87fe5203217309fbf7f352ba1143e2def7766
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-26 14:45:50 +0200

    nummer 9 på grejen
    gjorde 9an på primitiva tal grejen tog lite hjälp av internet med math.ceil vilket jag lärde mig av.

---
+++ src/primitivedatatypes/Numero9.java
@@ -0,0 +1,14 @@
+package primitivedatatypes;
+
+public class Numero9 {
+
+	public static void main(String[] args) {
+		float a = 4.5f;
+
+		int d = (int) Math.ceil(a);
+
+		System.out.println(d);
+        
+	}
+
+}
