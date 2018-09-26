0080930a0eb776824e8c463277a9e60ea37e35d5
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-09-26 14:23:12 +0200

    nummer 8
    tog hjälp från interwebs

--- src/variabler/Trialss.java
+++ src/variabler/Trialss.java
@@ -1,15 +1,23 @@
 package variabler;
-
+import java.util.*;
 public class Trialss {
 
 	public static void main(String[] args) {
 		System.out.println();
-		
-		float value = -45.34f;
-		
-		System.out.println(Math.abs(value));
-		
+		Scanner in = new Scanner(System.in);
+        System.out.print("Input a float number: 12.34 ");
+        float  x = in.nextFloat();  
+        System.out.printf("The absolute value of %.2f is: %.2f",x, convert(x));
+		System.out.printf("\n");
+    }
 
+    public static float convert(float n)
+	{
+		float absvalue = (n >= 0) ? n : -n;
+		return absvalue;
+		
+		
+		
 	}
 
 }
