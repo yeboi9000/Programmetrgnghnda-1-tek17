fc8f66b606ad0adb257afebb733ec6d58577e92a
yeboi9000 <42860837+yeboi9000@users.noreply.github.com>
2018-10-03 14:25:18 +0200

    input 3
    input 3 är klar men jag googlade lite om for osv och annat

--- src/inputoutput/Input3.java
+++ src/inputoutput/Input3.java
@@ -1,10 +1,23 @@
 package inputoutput;
-
+import java.util.Scanner;
 public class Input3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		   
+		  System.out.print("Input a number: ");
+		  int num1 = in.nextInt();
+		   
+		  for (int i=0; i< 10; i++){
+		   System.out.println(num1 + " x " + (i+1) + " = " + 
+		     (num1 * (i+1)));
+	
+		  
+		  
+		 }
+		}
+}		
 
-	}
+	
 
-}
+
